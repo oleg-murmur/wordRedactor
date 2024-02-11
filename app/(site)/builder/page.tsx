75be81029bb0ce242eba60ac9df4ff47709576b8
kipyusha <kirya16garchev@gmail.com>
2024-02-11 23:22:57 +0600

    add header

---
+++ app/(site)/builder/page.tsx
@@ -0,0 +1,13 @@
+import Header from "../../../components/Header";
+
+
+export default function Builder() {
+  return (
+    <main className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <Header>
+        <div className="absolute top-5 left-0 w-full flex items-center justify-center text-lg text-neutral-900">Конструктор</div>
+      </Header>
+      <div className="">Builder Content</div>
+    </main>
+  );
+}
