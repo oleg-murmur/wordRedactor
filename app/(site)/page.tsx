c31671205edfcac134a9843862b21f18e2c53d3a
kipyusha <kirya16garchev@gmail.com>
2024-02-12 14:52:57 +0600

    Unfinished main content

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Header from "../../components/Header";
+import PageContent from "./components/PageContent";
 
 
 export default function Home() {
@@ -7,7 +8,7 @@ export default function Home() {
       <Header>
         <div className="absolute top-5 left-0 w-full flex items-center justify-center text-lg text-neutral-900">Главная</div>
       </Header>
-      <div className="">Main Content</div>
+      <PageContent/>
     </main>
   );
 }
